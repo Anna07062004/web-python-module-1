82812300b09bdfd68686be2f5b9794a13ec6cd4a
Your Name <you@example.com>
2025-12-19 20:07:41 +0300

    feat: We started the first task

---
+++ task-1.py
@@ -0,0 +1,9 @@
+num = int(input("Введите число 1: "))
+num2 = int(input("Введите число 2: "))
+num3 = int(input("Введите число 3: "))
+
+sum = num + num2 + num3
+multip = num * num2 * num3
+
+print(f"Сумма: {sum}")
+print(f"Произведение: {multip}")
