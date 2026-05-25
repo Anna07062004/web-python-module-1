c42ea9f2bd65da6ab100fe1aed674aa7b64a7a2f
Anna07062004 <darrokof@gmail.com>
2026-05-25 20:44:35 +0300

    cw: изучение баз данных

--- module-17/practice/practice_1.sql
+++ module-17/practice/practice_1.sql
@@ -70,3 +70,115 @@ returning id,name,employee_id, budget,is_active;
 delete from projects  
 where is_active = false 
 returning id, name;
+
+----------------------------------------------------------
+
+create table employee_profiles (
+	id SERIAL primary key,
+	employee_id INT unique references employees (id),
+	phone TEXT unique,
+	address TEXT,
+	birth_data DATE
+);
+
+insert into employee_profiles(employee_id, phone, address, birth_data)
+values 
+	(1, '+70000000000', 'address-1', '1980-05-25'),
+	(2, '+70000000001', 'address-2', '1981-05-25'),
+	(3, '+70000000002', 'address-3', '1982-05-25');
+
+select 
+	e.name as employee_name,
+	ep.phone,
+	ep.address,
+	ep.birth_data
+from employees e
+join employee_profiles ep on ep.employee_id = e.id;
+
+insert into employee_profiles(employee_id, phone, address, birth_data)
+values
+	(1, '+70000000003', 'address-4', '1985-05-25');
+
+----------------------------------------------------------------------------
+
+-- N - N
+create table skills(
+	id SERIAL primary key,
+	name TEXT not null unique 
+);
+
+create table employee_skills(
+	employee_id INT references employees(id),
+	skill_id int references skills(id),
+	primary key (employee_id, skill_id)
+);
+
+insert into skills(name)
+values 
+	('SQL'),
+	('PostgreSQL'),
+	('MySQL'),
+	('Excel');
+
+insert into employee_skills(employee_id, skill_id)
+values 
+	(1,1),
+	(2,1),
+	(3,1),
+	(1,2),
+	(2,2),
+	(3,2),
+	(1,4);
+
+select 
+	e.name as employee_name,
+	s.name as skill_name
+from employee_skills es
+join employees e on es.employee_id = e.id
+join skills s on es.skill_id = s.id 
+order by e.name, s.name;
+
+---------------------------------------------------------------
+
+select 
+	e.name as employee_name,
+	e.salary,
+	d.name as department_name,
+	ep.phone,
+	ep.address,
+	p.name as project_name,
+	s.name as skill_name
+from employees e
+left join departments d on e.id = d.id
+left join employee_profiles ep on e.id = ep.id
+left join projects p on e.id = p.id
+left join employee_skills es on es.employee_id = e.id
+left join skills s on es.skill_id = s.id
+order by e.name, p.name, s.name
+
+----------------------------------------------------------------
+
+select 
+	e.name as employee_name,
+	coalesce(sum(p.budget), 0) as total_budget
+from employees e
+left join projects p on e.id = p.id
+group by e.id, e.name
+order by total_budget desc, e.name;
+
+----------------------------------------------------------------
+
+select 
+	p.name as project_name,
+	p.budget,
+	e.name as employee_name,
+	d.name as department_name
+from projects p
+join employees e  on p.employee_id = e.id
+join departments d on e.department_id = d.id
+where is_active = true 
+	and p.budget > 200000
+order by p.budget desc;
+
+-----------------------------------------------------------------
+
